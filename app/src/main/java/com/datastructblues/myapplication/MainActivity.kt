4aaeb764ad1d092e548f2e100ad6e0ed8a859f74
Umut Fidan <contact.umutfidan@gmail.com>
2022-08-07 01:17:13 +0300

    init

---
+++ app/src/main/java/com/datastructblues/myapplication/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.datastructblues.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.datastructblues.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    private lateinit var navController:NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = Navigation.findNavController(this,R.id.fragment)
+        setupWithNavController(binding.bottomNavigationView,navController)
+    }
+}
